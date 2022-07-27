6732d9cdcc054ce9d1421b66912bb050803f92d0
betameche111 <lolo.betameche@hotmail.fr>
2022-07-27 15:11:30 +0200

    Nouvelle valeur de notes

--- Fluto.ino
+++ Fluto.ino
@@ -10,20 +10,27 @@
 #define pinM1 26
 #define pinM2 25
 
-#define servoUpPosition 10
-#define servoDownPosition 30
+#define SERVO_UP_POSITION 0
+#define SERVO_DOWN_POSITION 30
+#define ESC_SPEED 9
+#define STEP_BY_SECOND 22000
+#define ACCELERATION_IN_STEP_PER_SECOND_2 1000000
 
-#define LA4 120
-#define LA4D 2500
-#define SI4 4700
-#define DO5 6800
-#define DO5D 8500
-#define RE5 10100
-#define RE5D 11800
-#define MI5 473
-#define FA5 519
-#define FA5D 561
-#define SOL5 612
+#define LA4 150
+#define LA4D 680
+#define SI4 1240
+#define DO5 1730
+#define DO5D 2200
+#define RE5 2630
+#define RE5D 3030
+#define MI5 3390
+#define FA5 3705
+#define FA5D 4025
+#define SOL5 4300
+#define SOL5D 4560
+#define LA5 4790
+#define LA5D 5000
+#define SI5 5200
 
 #define FULL_RANGE 670
 
@@ -36,12 +43,9 @@ AccelStepper stepper(AccelStepper::DRIVER, pinStep, pinDir);
 void setup()
 {
   Serial.begin(115200);
-  delay(5000);
-  Serial.println("Start");
-
   // Brushless Motor
-  // ESP32PWM::allocateTimer(0);
-  // ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   esc.setPeriodHertz(50); // standard 50 hz servo
@@ -51,64 +55,61 @@ void setup()
   // Servo
   servo.setPeriodHertz(50);
   servo.attach(pinServo, 1000, 2000);
-  servo.write(servoUpPosition);
+  servo.write(SERVO_UP_POSITION);
 
   pinMode(pinM0, OUTPUT);
   pinMode(pinM1, OUTPUT);
   pinMode(pinM2, OUTPUT);
   digitalWrite(pinM0, HIGH);
   digitalWrite(pinM1, HIGH);
-  digitalWrite(pinM2, HIGH);
+  digitalWrite(pinM2, LOW);
 
   pinMode(pinEnable, OUTPUT);
   digitalWrite(pinEnable, HIGH);
-  stepper.setMaxSpeed(100000);
-  stepper.setAcceleration(1000); // 100 steps/s²
+  stepper.setMaxSpeed(STEP_BY_SECOND);
+  stepper.setAcceleration(ACCELERATION_IN_STEP_PER_SECOND_2); // 100 steps/s²
 
-  servo.write(servoDownPosition);
-  Serial.println("End Setup");
+  servo.write(SERVO_DOWN_POSITION);
 }
 
 void start_song()
 {
-  // servo.write(servoDownPosition);
+  servo.write(SERVO_DOWN_POSITION);
   digitalWrite(pinEnable, LOW);
-  // digitalWrite(pinRelay, LOW);
+  esc.write(ESC_SPEED);
   delay(2000);
 }
 
 void stop_song()
 {
-  // servo.write(servoDownPosition);
+  servo.write(SERVO_DOWN_POSITION);
   digitalWrite(pinEnable, HIGH);
-  // digitalWrite(pinRelay, HIGH);
+  esc.write(0);
 }
 
 void play_note(uint32_t note, uint32_t duration)
 {
   static uint32_t lastNote = 0;
-  // stepper->moveTo(note);
-  //  servo.write(servoUpPosition);
+  stepper.runToNewPosition(note);
+  servo.write(SERVO_UP_POSITION);
   delay(duration);
   if (lastNote == note)
   {
-    // servo.write(servoDownPosition);
+    servo.write(SERVO_DOWN_POSITION);
     delay(50);
   }
   lastNote = note;
 }
 
-// void play_sncf()
-// {
-//   // servo.write(servoDownPosition);
-//   stepper->moveTo(LA4);
-//   delay(500);
-//   stepper->moveTo(MI5);
-//   delay(600);
-//   stepper->moveTo(FA5);
-//   delay(350);
-//   stepper->moveTo(DO5);
-// }
+void play_sncf()
+{
+  start_song();
+  play_note(LA4, 500);
+  play_note(MI5, 600);
+  play_note(FA5, 350);
+  play_note(DO5, 300);
+  stop_song();
+}
 
 void play_jurassik()
 {
@@ -165,38 +166,15 @@ void play_imperial_march()
   stop_song();
 }
 
-long motorValue = 9;
-
 void loop()
 {
   long value = Serial.parseInt();
-  if (value < 0)
+  if (value == 1)
   {
-    motorValue = -value;
+    play_jurassik();
   }
-  else if (value != 0)
+  else if (value == 2)
   {
-    digitalWrite(pinEnable, LOW);
-    Serial.print("Value : ");
-    Serial.println(value);
-    stepper.runToNewPosition(value);
-    servo.write(servoDownPosition);
-    esc.write(motorValue);
-    delay(5000);
-    servo.write(servoUpPosition);
-    delay(5000);
-    esc.write(0);
-    servo.write(servoDownPosition);
-    digitalWrite(pinEnable, HIGH);
+    play_sncf();
   }
-  // if (digitalRead(leftButton) == 0)
-  // {
-  //   play_imperial_march();
-  // }
-  // if (digitalRead(rightButton) == 0 && pos < FULL_RANGE)
-  // {
-  //   stepper->moveTo(0);
-  // }
-  // // stepper->moveTo(pos);
-  // delay(100);
 }
