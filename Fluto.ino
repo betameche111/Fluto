031acde1b88befbf9b244e2044bcc07cde6577a6
betameche111 <lolo.betameche@hotmail.fr>
2022-08-22 07:27:24 +0200

    Marche sans crash

--- Fluto.ino
+++ Fluto.ino
@@ -1,5 +1,13 @@
 #include <AccelStepper.h>
-#include <ESP32Servo.h>
+#include <Servo.h>
+
+// OTA
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+const char *ssid = "Chaco WiFi";
+const char *password = "Je suis une fourmis";
 
 #define pinEnable 14 // Activation du driver/pilote
 #define pinStep 33   // Signal de PAS (avancement)
@@ -9,12 +17,22 @@
 #define pinM0 27
 #define pinM1 26
 #define pinM2 25
+#define pinButtonUp 23
+#define pinButtonDown 18
+#define pinButtonEnter 17
+#define pinLedButtonUp 19
+#define pinLedButtonDown 5
+#define pinLedButtonEnter 16
+#define pinEndButton 2
 
 #define SERVO_UP_POSITION 0
 #define SERVO_DOWN_POSITION 30
 #define ESC_SPEED 9
-#define STEP_BY_SECOND 22000
-#define ACCELERATION_IN_STEP_PER_SECOND_2 1000000
+#define STEP_BY_SECOND 10000
+#define ACCELERATION_IN_STEP_PER_SECOND_2 100000
+
+#define LCD_ROWS 2
+#define LCD_COLS 16
 
 #define LA4 150
 #define LA4D 680
@@ -32,7 +50,7 @@
 #define LA5D 5000
 #define SI5 5200
 
-#define FULL_RANGE 670
+#define FULL_RANGE 5346
 
 Servo esc;
 Servo servo;
@@ -42,34 +60,72 @@ AccelStepper stepper(AccelStepper::DRIVER, pinStep, pinDir);
 
 void setup()
 {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.setHostname("Flute");
+
+  ArduinoOTA.begin();
+
   Serial.begin(115200);
-  // Brushless Motor
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  esc.setPeriodHertz(50); // standard 50 hz servo
-  esc.attach(pinESC, 1000, 2000);
+  //  Brushless Motor
+  esc.attach(pinESC);
   esc.write(0);
 
   // Servo
-  servo.setPeriodHertz(50);
   servo.attach(pinServo, 1000, 2000);
   servo.write(SERVO_UP_POSITION);
+  servo.write(SERVO_DOWN_POSITION);
 
+  // // StepperMotor
   pinMode(pinM0, OUTPUT);
   pinMode(pinM1, OUTPUT);
   pinMode(pinM2, OUTPUT);
+  pinMode(pinStep, OUTPUT);
+  pinMode(pinDir, OUTPUT);
   digitalWrite(pinM0, HIGH);
   digitalWrite(pinM1, HIGH);
   digitalWrite(pinM2, LOW);
-
   pinMode(pinEnable, OUTPUT);
   digitalWrite(pinEnable, HIGH);
   stepper.setMaxSpeed(STEP_BY_SECOND);
   stepper.setAcceleration(ACCELERATION_IN_STEP_PER_SECOND_2); // 100 steps/s²
 
-  servo.write(SERVO_DOWN_POSITION);
+  // Button
+  pinMode(pinButtonUp, INPUT_PULLUP);
+  pinMode(pinButtonDown, INPUT_PULLUP);
+  pinMode(pinButtonEnter, INPUT_PULLUP);
+  pinMode(pinLedButtonUp, OUTPUT);
+  pinMode(pinLedButtonDown, OUTPUT);
+  pinMode(pinLedButtonEnter, OUTPUT);
+  digitalWrite(pinLedButtonUp, HIGH);
+  digitalWrite(pinLedButtonDown, HIGH);
+  digitalWrite(pinLedButtonEnter, HIGH);
+
+  pinMode(pinEndButton, INPUT_PULLUP);
+
+  // Calibration
+  calibrate();
+}
+
+void calibrate()
+{
+  int position = 0;
+  digitalWrite(pinEnable, LOW);
+  while (digitalRead(pinEndButton) == HIGH)
+  {
+    position = position + 1;
+    stepper.runToNewPosition(position);
+    delay(1);
+  }
+  stepper.setCurrentPosition(FULL_RANGE);
+  digitalWrite(pinEnable, HIGH);
 }
 
 void start_song()
@@ -127,28 +183,20 @@ void play_jurassik()
   stop_song();
 }
 
-// void play_star_wars()
-// {
-//   start_song();
-//   delay(2000);
-//   stepper->moveTo(LA4);
-//   stepper->setSpeedInHz(1000);
-//   delay(1200);
-//   stepper->moveTo(RE5);
-//   stepper->setSpeedInHz(3000);
-//   delay(1200);
-//   stepper->moveTo(MI5);
-//   delay(1200);
-//   stepper->moveTo(FA5);
-//   delay(300);
-//   stepper->moveTo(SOL5);
-//   delay(300);
-//   stepper->moveTo(FA5);
-//   delay(1200);
-//   stepper->moveTo(LA4);
-//   delay(1000);
-//   stop_song();
-// }
+void play_star_wars()
+{
+  start_song();
+  play_note(LA4, 1200);
+  stepper.setMaxSpeed(STEP_BY_SECOND);
+  play_note(RE5, 1200);
+  stepper.setMaxSpeed(STEP_BY_SECOND);
+  play_note(MI5, 1200);
+  play_note(FA5, 300);
+  play_note(SOL5, 300);
+  play_note(FA5, 1200);
+  play_note(LA4, 1000);
+  stop_song();
+}
 
 void play_imperial_march()
 {
@@ -165,16 +213,18 @@ void play_imperial_march()
 
   stop_song();
 }
-
+int i = 0;
 void loop()
 {
-  long value = Serial.parseInt();
-  if (value == 1)
+  Serial.print("ok");
+  Serial.println(i);
+  i = i + 1;
+  delay(1000);
+  ArduinoOTA.handle();
+
+  if (digitalRead(pinButtonUp) == LOW)
   {
-    play_jurassik();
-  }
-  else if (value == 2)
-  {
-    play_sncf();
+    Serial.println("UP");
+    play_imperial_march();
   }
 }
